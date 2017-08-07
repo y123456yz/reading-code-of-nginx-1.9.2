d874d121a84fc343318c481d662cbadd27dbcce3
y123456yz <3058078489@qq.com>
2017-08-07 11:26:11 +0800

    change ngx_log_error_coreall

--- nginx-1.9.2/objs/ngx_modules.c
+++ nginx-1.9.2/objs/ngx_modules.c
@@ -11,13 +11,11 @@ extern ngx_module_t  ngx_events_module;
 extern ngx_module_t  ngx_event_core_module;
 extern ngx_module_t  ngx_epoll_module;
 extern ngx_module_t  ngx_thread_pool_module;
-extern ngx_module_t  ngx_openssl_module;
 extern ngx_module_t  ngx_regex_module;
 extern ngx_module_t  ngx_http_module;
 extern ngx_module_t  ngx_http_core_module;
 extern ngx_module_t  ngx_http_log_module;
 extern ngx_module_t  ngx_http_upstream_module;
-extern ngx_module_t  ngx_http_v2_module;
 extern ngx_module_t  ngx_http_static_module;
 extern ngx_module_t  ngx_http_autoindex_module;
 extern ngx_module_t  ngx_http_index_module;
@@ -30,7 +28,6 @@ extern ngx_module_t  ngx_http_map_module;
 extern ngx_module_t  ngx_http_split_clients_module;
 extern ngx_module_t  ngx_http_referer_module;
 extern ngx_module_t  ngx_http_rewrite_module;
-extern ngx_module_t  ngx_http_ssl_module;
 extern ngx_module_t  ngx_http_proxy_module;
 extern ngx_module_t  ngx_http_fastcgi_module;
 extern ngx_module_t  ngx_http_uwsgi_module;
@@ -53,7 +50,6 @@ extern ngx_module_t  ngx_http_redis2_module;
 extern ngx_module_t  ngx_http_write_filter_module;
 extern ngx_module_t  ngx_http_header_filter_module;
 extern ngx_module_t  ngx_http_chunked_filter_module;
-extern ngx_module_t  ngx_http_v2_filter_module;
 extern ngx_module_t  ngx_http_range_header_filter_module;
 extern ngx_module_t  ngx_http_gzip_filter_module;
 extern ngx_module_t  ngx_http_postpone_filter_module;
@@ -62,8 +58,6 @@ extern ngx_module_t  ngx_http_charset_filter_module;
 extern ngx_module_t  ngx_http_userid_filter_module;
 extern ngx_module_t  ngx_http_myfilter_module;
 extern ngx_module_t  ngx_http_headers_filter_module;
-extern ngx_module_t  ngx_http_echo_module;
-extern ngx_module_t  ngx_http_lua_module;
 extern ngx_module_t  ngx_http_copy_filter_module;
 extern ngx_module_t  ngx_http_range_body_filter_module;
 extern ngx_module_t  ngx_http_not_modified_filter_module;
@@ -76,13 +70,11 @@ ngx_module_t *ngx_modules[] = {
     &ngx_event_core_module,
     &ngx_epoll_module,
     &ngx_thread_pool_module,
-    &ngx_openssl_module,
     &ngx_regex_module,
     &ngx_http_module,
     &ngx_http_core_module,
     &ngx_http_log_module,
     &ngx_http_upstream_module,
-    &ngx_http_v2_module,
     &ngx_http_static_module,
     &ngx_http_autoindex_module,
     &ngx_http_index_module,
@@ -95,7 +87,6 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_split_clients_module,
     &ngx_http_referer_module,
     &ngx_http_rewrite_module,
-    &ngx_http_ssl_module,
     &ngx_http_proxy_module,
     &ngx_http_fastcgi_module,
     &ngx_http_uwsgi_module,
@@ -118,7 +109,6 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_write_filter_module,
     &ngx_http_header_filter_module,
     &ngx_http_chunked_filter_module,
-    &ngx_http_v2_filter_module,
     &ngx_http_range_header_filter_module,
     &ngx_http_gzip_filter_module,
     &ngx_http_postpone_filter_module,
@@ -127,8 +117,6 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_userid_filter_module,
     &ngx_http_myfilter_module,
     &ngx_http_headers_filter_module,
-    &ngx_http_echo_module,
-    &ngx_http_lua_module,
     &ngx_http_copy_filter_module,
     &ngx_http_range_body_filter_module,
     &ngx_http_not_modified_filter_module,
